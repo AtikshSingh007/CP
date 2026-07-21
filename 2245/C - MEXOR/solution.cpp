e02bc1daf669b732b51fe316f6fc3a0e983cbbf3
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-07-21 10:19:30 +0530

    Add MEXOR [C] from Codeforces

--- 2245/C - MEXOR/solution.cpp
+++ 2245/C - MEXOR/solution.cpp
@@ -1,70 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int bitLength(long long x) {
-    int cnt = 0;
-    while (x > 0) {
-        cnt++;
-        x >>= 1;
-    }
-    return cnt;
-}
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	
+	int n,k;
+	cin>>n>>k;
+	int y=k^n;
+	int msb=log2(n-1)+1;
+	if(( (1<<msb) -1 ) <y)
+	{
+	    cout<<"NO"<<endl;
+	    continue;
+	}
+	vector <int> last={0},start;
+	for(int i=0;i<32;i++)
+	if((1<<i)&y)last.push_back((1<<i));
+	int m=last.size();
+	int j=m-1;
+	
+	for(int i=n-1;i>=0;i--)
+	{
+	if(j>=0 && last[j]!=i){
+	start.push_back(i);
+	}
+	else j--;
+	}
+	cout<<"YES"<<endl;
+	for(auto i:start)cout<<i<<" ";
+	for(auto i:last)cout<<i<<" ";
+	cout<<endl;
+	}
  
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
- 
-    int t;
-    cin >> t;
-    while (t--) {
-        long long n, k;
-        cin >> n >> k;
- 
-        if (n == 1) {
-            if (k == 1) {
-                cout << "YES
-" << 0 << "
-";
-            } else {
-                cout << "NO
-";
-            }
-            continue;
-        }
- 
-        k ^= n;
-        if (bitLength(k) > bitLength(n - 1)) {
-            cout << "NO
-";
-            continue;
-        }
- 
-        vector<long long> s;
-        if (0 < k && k <= n - 1) {
-            s.push_back(k);
-        } else if (k) {
-            s.push_back(n - 1);
-            s.push_back((n - 1) ^ k);
-        }
-        s.push_back(0);
- 
-        unordered_set<long long> inS(s.begin(), s.end());
- 
-        vector<long long> a = s;
-        for (long long i = 0; i < n; i++) {
-            if (inS.find(i) == inS.end()) {
-                a.push_back(i);
-            }
-        }
- 
-        cout << "YES
-";
-        for (int i = (int)a.size() - 1; i >= 0; i--) {
-            cout << a[i];
-            if (i > 0) cout << ' ';
-        }
-        cout << "
-";
-    }
-    return 0;
 }
