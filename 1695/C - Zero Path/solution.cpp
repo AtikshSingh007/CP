7ad46425d7c5277e3c1dcc3d29b7897664575786
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-08-01 19:23:50 +0530

    Add Zero Path [C] from Codeforces

---
+++ 1695/C - Zero Path/solution.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef __gnu_pbds::tree<int,__gnu_pbds::null_type,less<int>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update> ordered_set;
+vector<long long> tree;
+void build(auto &a, long long  v, long long  tl, long long  tr) {
+    if (tl == tr) {
+        tree[v] = a[tl];
+        return;
+    }
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    build(a, left, tl, tm);
+    build(a, right, tm + 1, tr);
+    tree[v] = __gcd(tree[left], tree[right]);
+}
+ 
+long long query(int v, int tl, int tr, int l, int r) {
+    if (l <= tl && r >= tr) return tree[v];
+    if (tl > r || tr < l) return 0;
+    int tm = (tl + tr) / 2;
+    long long res_left = query(2 * v + 1, tl, tm, l, r);
+    long long res_right = query(2 * v + 2, tm + 1, tr, l, r);
+    return __gcd(res_left, res_right);
+}
+void update(int tl, int tr, int v, int pos, int val) {
+    if (tl == tr) {
+        tree[v] = val;
+        return;
+    }
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    if (pos <= tm) {
+        update(tl, tm, left, pos, val);
+    } else {
+        update(tm + 1, tr, right, pos, val);
+    }
+    tree[v] = __gcd(tree[left], tree[right]);
+}
+#define MOD 998244353
+#define bit_count __builtin_popcountll
+#define Atiksh ios_base::sync_with_stdio(false);cin.tie(NULL);
+ 
+ 
+ 
+ 
+void solve(){
+int n,m;
+cin>>n>>m;
+ 
+vector <vector<int> > mx(n,vector<int> (m,-1e9)) , mn(n,vector <int> (m,1e9) );
+auto a=mx;
+for(int i=0;i<n;i++)
+    for(int j=0;j<m;j++)
+    cin>>a[i][j];
+ 
+    mx[0][0]=mn[0][0]=a[0][0];
+for(int i=0;i<n;i++)
+    for(int j=0;j<m;j++)
+    {
+        if(i>0){
+            mx[i][j]=max(a[i][j]+mx[i-1][j],mx[i][j]);
+            mn[i][j]=min(a[i][j]+mn[i-1][j],mn[i][j]);
+        }
+        if(j>0)
+        {
+            mx[i][j]=max(a[i][j]+mx[i][j-1],mx[i][j]);
+            mn[i][j]=min(a[i][j]+mn[i][j-1],mn[i][j]);
+        }
+    }
+ 
+if( (n+m)&1 && mx[n-1][m-1]>=0 && mn[n-1][m-1]<=0 )
+    cout<<"YES"<<endl;
+else
+    cout<<"NO"<<endl;
+    //cout<<mx[n-1][m-1]<< " ojdf  "<<mn[n-1][m-1]<<endl;
+ 
+ 
+}
+ 
+ 
+ 
+int main() {
+    Atiksh
+    int t;
+    t=1;
+    cin>>t;
+ 
+        while (t--) {
+            solve();
+        }
+ 
+    return 0;
+}
