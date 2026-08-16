9f14426c821badb6271b431417ea7f90e4accc60
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-08-16 23:51:23 +0530

    Add Training Session [D] from Codeforces

---
+++ 1598/D - Training Session/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); 
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), ca(n + 1), cb(n + 1);
+    for (int i = 0; i < n; ++i) {
+      cin >> a[i] >> b[i];
+      ca[a[i]]++; cb[b[i]]++;
+    }
+    long long ans = n * 1LL * (n - 1) * (n - 2) / 6;
+    for (int i = 0; i < n; ++i) 
+      ans -= (ca[a[i]] - 1) * 1LL * (cb[b[i]] - 1);
+    cout << ans << '
+';
+  }
+} 
