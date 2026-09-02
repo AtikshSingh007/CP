81b88e3c0641e0dec3014fbd8c9f0866cb827b74
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-09-03 00:17:05 +0530

    Add Carrot Chopdown (Easy Version) [B1] from Codeforces

--- 2258/B1 - Carrot Chopdown (Easy Version)/solution.cpp
+++ 2258/B1 - Carrot Chopdown (Easy Version)/solution.cpp
@@ -1,45 +1,82 @@
 #include <bits/stdc++.h>
- 
-using i64 = long long;
- 
-void solve() {
-    int N, M;
-    std::cin >> N >> M;
- 
-    std::vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        std::cin >> A[i];
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef __gnu_pbds::tree<int,__gnu_pbds::null_type,less<int>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update> ordered_set;
+vector<long long> tree;
+void build(auto &a, long long  v, long long  tl, long long  tr) {
+    if (tl == tr) {
+        tree[v] = a[tl];
+        return;
     }
- 
-    std::vector<int> sf(M + 2);
-    for (int i = 0; i < N; ++i) {
-        sf[A[i]] += 1;
-    }
- 
-    for (int i = M; i >= 1; --i) {
-        sf[i - 1] += sf[i];
-    }
- 
-    i64 ans = 0;
-    for (int x = 1; x <= M; ++x) {
-        i64 now = sf[x];
-        if (x * 2 <= M) now += sf[2 * x] - sf[2 * x + 1];
-        ans = std::max(ans, now);
-    }
-    std::cout << ans << '
-';
-    
-    return;
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    build(a, left, tl, tm);
+    build(a, right, tm + 1, tr);
+    tree[v] = __gcd(tree[left], tree[right]);
 }
  
-int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
- 
-    int TT = 1; std::cin >> TT;
-    for (int i = 1; i <= TT; ++i) {
-        solve();
+long long query(int v, int tl, int tr, int l, int r) {
+    if (l <= tl && r >= tr) return tree[v];
+    if (tl > r || tr < l) return 0;
+    int tm = (tl + tr) / 2;
+    long long res_left = query(2 * v + 1, tl, tm, l, r);
+    long long res_right = query(2 * v + 2, tm + 1, tr, l, r);
+    return __gcd(res_left, res_right);
+}
+void update(int tl, int tr, int v, int pos, int val) {
+    if (tl == tr) {
+        tree[v] = val;
+        return;
     }
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    if (pos <= tm) {
+        update(tl, tm, left, pos, val);
+    } else {
+        update(tm + 1, tr, right, pos, val);
+    }
+    tree[v] = __gcd(tree[left], tree[right]);
+}
+#define MOD 998244353
+#define bit_count __builtin_popcountll
+#define Atiksh ios_base::sync_with_stdio(false);cin.tie(NULL);
+//vector <int> sqr;
+ 
+void solve(){
+int n,m;
+cin>>n>>m;
+vector<int> a(n);
+for(int i=0;i<n;i++)cin>>a[i];
+unordered_map <int,int> mp;
+sort(a.begin(),a.end());
+ 
+int ans=0;
+for(auto i:a)mp[i]++;
+for(int i=1;i<=m;i++)
+{
+    int temp=n-(lower_bound(a.begin(),a.end(),i)-a.begin());
+    if(mp.count(2*i))temp+=mp[2*i];
+    ans=max(ans,temp);
+}
+cout<<ans<<endl;
+ 
+}
+ 
+ 
+ 
+int main() {
+    Atiksh
+ 
+ 
+    int t;
+    //t=1;
+    cin>>t;
+        while (t--) {
+            solve();
+        }
  
     return 0;
 }
