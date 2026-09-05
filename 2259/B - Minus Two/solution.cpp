d65096340f505d5388b90206040f8efee724d230
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-09-05 20:32:47 +0530

    Add Minus Two [B] from Codeforces

---
+++ 2259/B - Minus Two/solution.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+typedef __gnu_pbds::tree<int,__gnu_pbds::null_type,less<int>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update> ordered_set;
+vector<long long> tree;
+void build(auto &a, long long  v, long long  tl, long long  tr) {
+    if (tl == tr) {
+        tree[v] = a[tl];
+        return;
+    }
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    build(a, left, tl, tm);
+    build(a, right, tm + 1, tr);
+    tree[v] = __gcd(tree[left], tree[right]);
+}
+ 
+long long query(int v, int tl, int tr, int l, int r) {
+    if (l <= tl && r >= tr) return tree[v];
+    if (tl > r || tr < l) return 0;
+    int tm = (tl + tr) / 2;
+    long long res_left = query(2 * v + 1, tl, tm, l, r);
+    long long res_right = query(2 * v + 2, tm + 1, tr, l, r);
+    return __gcd(res_left, res_right);
+}
+void update(int tl, int tr, int v, int pos, int val) {
+    if (tl == tr) {
+        tree[v] = val;
+        return;
+    }
+    int left = 2 * v + 1;
+    int right = 2 * v + 2;
+    int tm = (tl + tr) / 2;
+    if (pos <= tm) {
+        update(tl, tm, left, pos, val);
+    } else {
+        update(tm + 1, tr, right, pos, val);
+    }
+    tree[v] = __gcd(tree[left], tree[right]);
+}
+#define MOD 998244353
+#define bit_count __builtin_popcountll
+#define Atiksh ios_base::sync_with_stdio(false);cin.tie(NULL);
+//vector <int> sqr;
+ 
+void solve(){
+   int n;
+   cin>>n;
+   vector<int> a(n);
+   for(int i=0;i<n;i++)cin>>a[i];
+   int odd=0,even1=0,even2=0;
+   for(int i=0;i<n;i++)
+   {
+       if(a[i]&1)odd++;
+       else
+       {
+           if( (a[i]/2)%2==1)
+            even1++;
+           else
+            even2++;
+       }
+   }
+   cout<<max(odd,max(even1,even2))<<endl;
+ 
+}
+ 
+ 
+ 
+int main() {
+    Atiksh
+ 
+ 
+    int t;
+    //t=1;
+    cin>>t;
+        while (t--) {
+            solve();
+        }
+ 
+    return 0;
+}
