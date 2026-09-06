265959c1e06e089b07566c5e6fa2c55d6654e33c
AtikshSingh007 <atikshsingh.e@gmail.com>
2026-09-07 00:21:50 +0530

    Add MEX Multiset [D] from Codeforces

--- 2259/D - MEX Multiset/solution.cpp
+++ 2259/D - MEX Multiset/solution.cpp
@@ -9,64 +9,44 @@ typedef __gnu_pbds::tree<int,__gnu_pbds::null_type,less<int>,__gnu_pbds::rb_tree
 #define Atiksh ios_base::sync_with_stdio(false);cin.tie(NULL);
  
 void solve(){
-    int n;
-    cin>>n;
-    vector<int>nums(n);
-    for(int i=0;i<n;i++)cin>>nums[i];
-    vector<int > a,b,c;
-    map<int,vector<int>> mp;
+ int n;
+ cin>>n;
  
-    for(int i=0;i<n;i++)mp[nums[i]].push_back(i);
+ vector<int> a(n);
+ for(int i=0;i<n;i++)cin>>a[i];
+ vector<int> zero;
  
-    int M = 0;
-    while(mp.count(M) && mp[M].size() >= 2) M++;
+ for(int i=0;i<n;i++)
+    if(a[i]==0)
+    zero.push_back(i);
  
-    for(auto& [k,vec]:mp)
-    {
-        if (k < M) {
-            a.push_back(vec.back());
-            vec.pop_back();
  
-            b.push_back(vec.back());
-            vec.pop_back();
+ if(zero.size()==1)
+ {
+  cout<<"NO"<<endl;
+  return;
+ }
  
-            while(vec.size()) {
-                a.push_back(vec.back());
-                vec.pop_back();
-            }
-        } else {
-            while(vec.size()) {
-                c.push_back(vec.back());
-                vec.pop_back();
-            }
-        }
-    }
  
-    int amex=-1,bmex=-1,cmex=-1;
-    for(auto i:a)if(nums[i]==(amex+1))amex++;
-    for(auto i:b)if(nums[i]==(bmex+1))bmex++;
-    for(auto i:c)if(nums[i]==(cmex+1))cmex++;
-    string ans(n,'_');
-    for(auto i:a)ans[i]='A';
-    for(auto i:c)ans[i]='C';
-    for(auto i:b)ans[i]='B';
-    amex++;
-    bmex++;
-    cmex++;
-    vector <int> re={amex,bmex,cmex};
-    sort(re.begin(),re.end());
-    amex=re.back();
-    bmex=re[1];
-    cmex=re[0];
+ string ans(n,'_');
  
-    if(amex<=(bmex+cmex))
-    {
-        cout<<"YES"<<endl;
-        for(auto i:ans)cout<<i;
-        cout<<endl;
-    }
-    else
-        cout<<"NO"<<endl;
+ if(zero.size()){
+ ans[zero.back()]='A';
+ zero.pop_back();
+ }
+ 
+ //cout<<1<<endl;
+ 
+ while(zero.size())
+ {
+     ans[zero.back()]='B';
+     zero.pop_back();
+ }
+//cout<<2<<endl;
+ for(int i=0;i<n;i++)
+    if(ans[i]=='_')ans[i]='C';
+ 
+ cout<<"YES"<<endl<<ans<<endl;
  
 }
  
